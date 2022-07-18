7f989761c0f27fa99e080983e4d48c7906f22da2
Willyan Fernando <Wff.cirino@gmail.com>
2022-07-17 21:55:06 -0300

    Ajuste eureka

--- src/main/java/com/clinica/ClinicaApplication.java
+++ src/main/java/com/clinica/ClinicaApplication.java
@@ -3,8 +3,10 @@ package com.clinica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClinicaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
