6fe4d34ac211d1988600a00d1c2ca9f929354554
Willyan Fernando <Wff.cirino@gmail.com>
2022-07-25 19:19:08 -0300

    Ajuste no swagger

--- src/main/java/com/clinica/ClinicaApplication.java
+++ src/main/java/com/clinica/ClinicaApplication.java
@@ -3,12 +3,13 @@ package com.clinica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ClinicaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
